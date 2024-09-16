13c681302f4fa6d835ff3be3d0b42b78f24f6ede
ashique <ashique12009@gmail.com>
2024-09-16 20:49:56 +0300

    is authentication middleware added, as before creating blog, an user must be logged in

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { login, register, logout } from '../controllers/user.controller.js';
+import { isAuthenticated } from '../middleware/authUser.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.post('/logout', logout);
+router.post('/logout', isAuthenticated, logout);
 
 export default router;
