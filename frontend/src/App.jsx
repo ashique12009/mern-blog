186fb079865079b2e9f479af023980aba3271bb4
ashique <ashique12009@gmail.com>
2024-09-17 18:13:26 +0300

    frontend router setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Blogs from './pages/Blogs'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Footer from './components/Footer'
+import { Routes, Route, useLocation } from 'react-router-dom'
 
 const App = () => {
+
+    const location = useLocation()
+    const hideNavbarFooter = ['/dashboard', '/login', '/register'].includes(location.pathname)
+
     return (
-        <div className='bg-red-500'>App</div>
+        <div>
+            {/* Conditional rendering */}
+            { !hideNavbarFooter && <Navbar /> }
+            
+            {/* Define routes */}
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route exact path="/blogs" element={<Blogs />} />
+                <Route exact path="/about" element={<About />} />
+                <Route exact path="/contact" element={<Contact />} />
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/register" element={<Register />} />
+            </Routes>
+            
+            {/* Conditional rendering */}
+            { !hideNavbarFooter && <Footer /> }
+        </div>
     )
 }
 
