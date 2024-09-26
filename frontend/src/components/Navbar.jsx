29454a0eab730613e0414e9bf9aa6b457c391f19
ashique <ashique12009@gmail.com>
2024-09-26 13:26:23 +0300

    set router, mobile menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,30 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from '../context/AuthProvider'
 import { Link } from 'react-router-dom'
+import { IoMenuSharp, IoCloseSharp } from 'react-icons/io5'
 
 const Navbar = () => {
 
-    const blogs = useAuth()
+    const {users, blogs} = useAuth()
+    const [show, setShow] = useState(false)
 
     return (
         <>
             <nav className='shadow-lg px-4 py-2'>
-                <div className='flex justify-between container mx-auto'>
+                <div className='flex items-center justify-between container mx-auto'>
                     <div className='font-semibold text-xl'>My <span className='text-blue-500'>Blog</span></div>
-                    <div>
-                        <ul className='flex space-x-6'>
+                    <div className='mx-6'>
+                        {/* Desktop */}
+                        <ul className='space-x-6 hidden md:flex'>
                             <Link to='/'><li>HOME</li></Link>
                             <Link to='/blogs'><li>BLOGS</li></Link>
                             <Link to='/creators'><li>CREATORS</li></Link>
                             <Link to='/about'><li>ABOUT</li></Link>
                             <Link to='/contact'><li>CONTACT</li></Link>
                         </ul>
+
+                        {/* Mobile */}
+                        <div className='md:hidden' onClick={() => setShow(!show)}>
+                            {
+                                show ? <IoCloseSharp size={24} /> : <IoMenuSharp size={24} />
+                            }
+                        </div>
                     </div>
-                    <div className='flex space-x-4'>
+                    <div className='space-x-4 hidden md:flex'>
                         <Link to='/dashboard'><button className='bg-blue-500 text-white px-4 py-1 rounded-md'>Dashboard</button></Link>
                         <Link to='/login'><button className='bg-red-500 text-white px-4 py-1 rounded-md'>Login</button></Link>
                     </div>
                 </div>
+                {/* Mobile navbar */}
+                {
+                    show && (
+                        <div className='bg-white'>
+                            <ul className='flex md:hidden text-xl flex-col h-screen items-center justify-center space-y-3'>
+                                <Link to='/' onClick={() => setShow(!show)}><li>HOME</li></Link>
+                                <Link to='/blogs' onClick={() => setShow(!show)}><li>BLOGS</li></Link>
+                                <Link to='/creators' onClick={() => setShow(!show)}><li>CREATORS</li></Link>
+                                <Link to='/about' onClick={() => setShow(!show)}><li>ABOUT</li></Link>
+                                <Link to='/contact' onClick={() => setShow(!show)}><li>CONTACT</li></Link>
+                            </ul>
+                        </div>
+                    )
+                }
             </nav>
         </>
     )
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [password, setPassword] = useState('')
+    const [role, setRole] = useState('')
+    const [education, setEducation] = useState('')
+    const [photo, setPhoto] = useState('')
+    const [photoPreview, setPhotoPreview] = useState('')
+
+    return (
+        <>
+        <form action=''>
+            <div className='font-semibold text-xl items-center text-center'>Blog</div>
+            <h1 className='text-xl font-semibold mb-6'>Register</h1>
+            <select>
+                <option value="">Select Role</option>
+                <option value="">Admin</option>
+                <option value="">User</option>
+            </select>
+        </form>
+        </>
+    )
 }
 
 export default Register
